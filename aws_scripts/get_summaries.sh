5a20a2acac495243d5e27cb52f750cf113c6c50f
Peter Hawkins <27868344+pdghawk@users.noreply.github.com>
2019-03-19 21:06:53 -0400

    added the aws scripts into the new dir structure

---
+++ aws_scripts/get_summaries.sh
@@ -0,0 +1,32 @@
+echo "getting tensorboard summaries from an AWS EC2 instance"
+
+echo "using the key:"
+echo "$1"
+
+echo "to access the instance:"
+echo "$2"
+
+DATE=`date +%Y-%m-%d`
+TIME=`date +%H%M`
+
+echo $DATE
+echo $TIME
+
+echo scp -i ~/.ssh/"$1" -r ubuntu@"$2":/home/ubuntu/deep-pong/data_summaries ../aws_runs/"$DATE"/"$TIME"
+
+
+DIR0="aws_runs/"$DATE""
+echo $DIR0
+
+if [ ! -d "$DIR0" ]; then
+  mkdir $DIR0
+fi
+
+DIR1=""$DIR0"/"$TIME""
+echo $DIR1
+
+if [ ! -d "$DIR1" ]; then
+  mkdir $DIR1
+fi
+
+scp -i ~/.ssh/"$1" -r ubuntu@"$2":/home/ubuntu/deep-pong/data_summaries "$DIR1"
---
+++ aws_scripts/upload_code.sh
@@ -0,0 +1,13 @@
+echo "uploading code to an AWS EC2 instance"
+
+echo "one should make the directory deep-pong/code available on the instance before running"
+
+echo "using the key:"
+echo "$1"
+
+echo "to access the instance:"
+echo "$2"
+
+scp -i ~/.ssh/"$1" -r ../code/DQN ubuntu@"$2":/home/ubuntu/deep-pong/code/DQN
+scp -i ~/.ssh/"$1" ../code/simple_loops.py ubuntu@"$2":/home/ubuntu/deep-pong/code
+scp -i ~/.ssh/"$1" ../code/play_from_ckpt.py ubuntu@"$2":/home/ubuntu/deep-pong/code
